368c7ad389b8b5550f8310fed78dbdde34d144ee
Aruming <0430kar@naver.com>
2022-09-26 14:13:58 +0900

    장바구니 품목을 주문, 주문 후 장바구니 비우기

--- src/main/java/com/ll/exam/spring_batch/app/cart/repository/CartItemRepository.java
+++ src/main/java/com/ll/exam/spring_batch/app/cart/repository/CartItemRepository.java
@@ -5,8 +5,11 @@ import com.ll.exam.spring_batch.app.member.entity.Member;
 import com.ll.exam.spring_batch.app.product.entity.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
     Optional<CartItem> findCartItemByMemberAndProductOption(Member member, ProductOption productOption);
+
+    List<CartItem> findAllByMemberId(Long memberId);
 }
--- src/main/java/com/ll/exam/spring_batch/app/cart/service/CartService.java
+++ src/main/java/com/ll/exam/spring_batch/app/cart/service/CartService.java
@@ -7,6 +7,7 @@ import com.ll.exam.spring_batch.app.product.entity.ProductOption;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -31,5 +32,13 @@ public class CartService {
 
             cartItemRepository.save(cartItem);
         }
+    }
+
+    public List<CartItem> getItemsByMember(Member member) {
+        return cartItemRepository.findAllByMemberId(member.getId());
+    }
+
+    public void deleteItem(CartItem cartItem) {
+        cartItemRepository.delete(cartItem);
     }
 }
