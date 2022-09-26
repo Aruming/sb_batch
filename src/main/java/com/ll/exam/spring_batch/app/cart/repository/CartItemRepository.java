c614b6e81d7db5675dec631a96c7b3cc05680e18
Aruming <0430kar@naver.com>
2022-09-26 13:27:45 +0900

    장바구니에 한 회원이 같은 상품 옵션을 2번 이상 넣으면 기존 아이템의 수량만 변화

--- src/main/java/com/ll/exam/spring_batch/app/cart/repository/CartItemRepository.java
+++ src/main/java/com/ll/exam/spring_batch/app/cart/repository/CartItemRepository.java
@@ -1,7 +1,12 @@
 package com.ll.exam.spring_batch.app.cart.repository;
 
 import com.ll.exam.spring_batch.app.cart.entity.CartItem;
+import com.ll.exam.spring_batch.app.member.entity.Member;
+import com.ll.exam.spring_batch.app.product.entity.ProductOption;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+    Optional<CartItem> findCartItemByMemberAndProductOption(Member member, ProductOption productOption);
 }
--- src/main/java/com/ll/exam/spring_batch/app/cart/service/CartService.java
+++ src/main/java/com/ll/exam/spring_batch/app/cart/service/CartService.java
@@ -7,18 +7,29 @@ import com.ll.exam.spring_batch.app.product.entity.ProductOption;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class CartService {
     private final CartItemRepository cartItemRepository;
 
     public void addItem(Member member, ProductOption productOption, int quantity) {
-        CartItem cartItem = CartItem.builder()
-                .member(member)
-                .productOption(productOption)
-                .quantity(quantity)
-                .build();
+        Optional<CartItem> item = cartItemRepository.findCartItemByMemberAndProductOption(member, productOption);
 
-        cartItemRepository.save(cartItem);
+        if (item.isPresent()){
+            CartItem cartItem = item.get();
+            cartItem.setQuantity(cartItem.getQuantity()+quantity);
+            cartItemRepository.save(cartItem);
+        }
+        else{
+            CartItem cartItem = CartItem.builder()
+                    .member(member)
+                    .productOption(productOption)
+                    .quantity(quantity)
+                    .build();
+
+            cartItemRepository.save(cartItem);
+        }
     }
 }
