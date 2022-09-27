8138e13f179a25dd8d73ebb14af9d4d56a06b9fc
Aruming <0430kar@naver.com>
2022-09-27 10:50:05 +0900

    예치금(Cash) 개념 도입

--- src/main/java/com/ll/exam/spring_batch/app/member/service/MemberService.java
+++ src/main/java/com/ll/exam/spring_batch/app/member/service/MemberService.java
@@ -1,16 +1,22 @@
 package com.ll.exam.spring_batch.app.member.service;
 
+import com.ll.exam.spring_batch.app.cash.entity.CashLog;
+import com.ll.exam.spring_batch.app.cash.service.CashService;
 import com.ll.exam.spring_batch.app.member.entity.Member;
 import com.ll.exam.spring_batch.app.member.repository.MemberRepository;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class MemberService {
     private final MemberRepository memberRepository;
+    private final CashService cashService;
 
+    @Transactional
     public Member join(String username, String password, String email) {
         Member member = Member
                 .builder()
@@ -22,5 +28,20 @@ public class MemberService {
         memberRepository.save(member);
 
         return member;
+    }
+
+    @Transactional
+    public long addCash(Member member, long price) {
+        CashLog cashLog = cashService.addCash(member, price);
+
+        long newRestCash = member.getRestCash() + cashLog.getPrice();
+        member.setRestCash(newRestCash);
+        memberRepository.save(member);
+
+        return newRestCash;
+    }
+
+    public long getRestCash(Member member) {
+        return member.getRestCash();
     }
 }
